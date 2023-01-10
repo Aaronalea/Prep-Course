324f4c95e3364284eb153bb478d190fb4a1b7bf6
aaronalea <aaronalearias@gmail.com>
2023-01-10 19:55:47 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "mundo";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 1000;
 
 // Crea una variable booleana:
 const nuevoBool = null;
